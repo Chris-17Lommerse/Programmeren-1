b5f7009a474a124570c1c6ea7c9984e2ead3ee73
Chris-17Lommerse <89691283+Chris-17Lommerse@users.noreply.github.com>
2021-09-20 12:43:15 +0200

    Versie 1

---
+++ Projecten/Projecten/script.js
@@ -0,0 +1,5 @@
+function myFunction() { /* Chris Lommerse, AEITO21SD-B */ 
+document.getElementsByTagname("P")[0].innerHTML = "Hello World!";
+}
+// Change heading:  document.getElementById("myH").innerHTML = "Chris Lommerse, AEITO21SD-B";
+//
